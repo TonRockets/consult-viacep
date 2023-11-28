141aa42cb3487c93432c8fa45c9211905074fdfa
TonRockets <wellington.pereira@al.forsoft.org.br>
2023-11-28 18:04:05 -0300

    feat(get-cep): create a function to consult cep in viacep api
    Related to GoStudy-003

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
+
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
 
 func main() {
 
@@ -14,4 +29,24 @@ func main() {
 			fmt.Printf("Erro ao iniciar o servidor!\n %v", err)
 		}
 	}()
+}
+
+func BuscaCEP(cep string) (*ViaCEP, error) {
+	res, err := http.Get("http://viacep.com.br/ws/" + cep + "/json/")
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var viaCEP ViaCEP
+	err = json.Unmarshal(body, &viaCEP)
+	if err != nil {
+		return nil, err
+	}
+	return &viaCEP, nil
 }
